436e869bae073ee8de961c474fde2c5a2baf9d47
Melissa Lieffers <mealieff@login1.hsn0.cm.bigred200.uits.iu.edu>
2025-01-26 11:45:48 -0500

    updated

--- getdistrib.py
+++ getdistrib.py
@@ -5,15 +5,28 @@ TEST = "ghc_test.tsv"
 
 
 print("Loading dataset...")
-data = pd.read_csv(INPUT_FILE, sep='\t')
+test_data = pd.read_csv(TRAIN, sep='\t')
+train_data = pd.read_csv(TEST, sep='\t')
 
 # Ensure the TSV file has the expected structure
 if 'text' not in data.columns or not {'hd', 'cv', 'vo'}.issubset(data.columns):
     raise ValueError("The input file must contain 'text', 'hd', 'cv', and 'vo' columns.")
 
 # Separate text and labels
-texts = data['text']
-binary_labels = data[['hd', 'cv', 'vo']].values
+train_texts = train_data['text']
+test_texts = test_data['text']
+train_labels = train_data[['hd', 'cv', 'vo']].values
+test_labels = test_data[['hd', 'cv', 'vo']].values
+
+# Extract binary labels per test/train file
+# Binary nonhate  = [000] or [001]
+# Binary hate = everything else
+
+
+
+
+# Extract multiclass labels per test/train file
+# Multiclass labels = all iterations in the range of [000]-[111]
 
 for i in range(0, len(texts)
 
